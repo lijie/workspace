20b56d85d5485792fbaadb07424e88114d75158b
LiJie <eltshanli@gmail.com>
2015-08-31 19:37:33 +0800

    自动安装go环境,更新emacs配置

--- install.sh
+++ install.sh
@@ -13,6 +13,38 @@ mkdir -p deps
 # install go mode for emacs
 cd deps
 
+# install Go
+GOVERSION=`go version 2>/dev/null`
+# install Go1.4.2
+if [ -z "$GOVERSION" ]; then
+    if [ ! -e go1.4.2.src.tar.gz ]; then
+	wget https://storage.googleapis.com/golang/go1.4.2.src.tar.gz
+    fi
+    if [ ! -d go1.4.2 ]; then
+	tar zxf go1.4.2.src.tar.gz
+	mv go go1.4.2
+    fi
+    if [ ! -e go1.4.2/bin/go ]; then
+	(cd go1.4.2/src/; ./make.bash)
+    fi
+fi
+export GOROOT_BOOTSTRAP=`pwd`/go1.4.2
+# install Go1.5
+if [ -z "$GOVERSION" ]; then
+    if [ ! -e go1.5.src.tar.gz ]; then
+	wget https://storage.googleapis.com/golang/go1.5.src.tar.gz
+    fi
+    if [ ! -d go1.5 ]; then
+	tar zxf go1.5.src.tar.gz
+	mv go go1.5
+    fi
+    if [ ! -e go1.5/bin/go ]; then
+	(cd go1.5/src/; ./make.bash)
+    fi
+fi
+GOBINPATH=`pwd`/go1.5/bin
+export PATH=$GOBINPATH:$PATH
+
 export GOPATH=`pwd`
 
 if [ -e go-mode.el ]; then
@@ -39,3 +71,6 @@ else
     go get $ORACLE
 fi
        
+# and last
+echo "step1: Put $GOBINPATH to you PATH"
+echo "step2: Copy deps/bin/godef, deps/bin/oracle to you Path"
